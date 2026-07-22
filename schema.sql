39a8d664c6a1c58db514fc31b86418bdb7a2451b
Steffi dorgus <steffidorgus.official@gmail.com>
2026-07-22 12:36:41 +0530

    Add deleted items table back in schema.sql

--- schema.sql
+++ schema.sql
@@ -10,3 +10,25 @@ CREATE TABLE IF NOT EXISTS locations (
     created_at TIMESTAMP DEFAULT NOW()
 );
 
+CREATE TABLE IF NOT EXISTS items (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name TEXT NOT NULL,
+    quantity INTEGER NOT NULL CHECK (quantity >= 0),
+
+    category_id BIGINT NOT NULL,
+    location_id BIGINT NOT NULL,
+
+    created_at TIMESTAMP DEFAULT NOW(),
+
+    CONSTRAINT unique_item UNIQUE (name, category_id, location_id),
+
+    CONSTRAINT fk_category
+        FOREIGN KEY (category_id)
+        REFERENCES categories(id)
+        ON DELETE RESTRICT,
+
+    CONSTRAINT fk_location
+        FOREIGN KEY (location_id)
+        REFERENCES locations(id)
+        ON DELETE RESTRICT
+);
